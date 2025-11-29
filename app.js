766c44163d5b0a1f3971117b7e727e4379f39dd3
shubhamparbhane18 <shubhamparbhane18@gmail.com>
2025-11-29 23:53:21 +0530

    feat: add SHOW ROUTE to read the data

--- app.js
+++ app.js
@@ -21,15 +21,23 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
 
 
 app.get("/",(req,res)=>{
   res.send("hi,I am robot");
 });
-
+//index route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index", { allListings });
+});
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+  let {id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listings/show.ejs",{listing} );
 });
 
 /*
