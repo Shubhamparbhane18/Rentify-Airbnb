c562133af1330ef57b6d5cceea99f91f0d1744a7
shubhamparbhane18 <shubhamparbhane18@gmail.com>
2026-01-03 14:00:25 +0530

    feat: add client-side validation for listing form using Bootstrap

--- models/listing.js
+++ models/listing.js
@@ -17,7 +17,7 @@ const listingSchema = new Schema({
     },
     url: {
       type: String,
-      default: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
+      default: "https://images.unsplash.com/photo-1586810724476-c294fb7ac01b?q=80&w=436&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
   },
 
